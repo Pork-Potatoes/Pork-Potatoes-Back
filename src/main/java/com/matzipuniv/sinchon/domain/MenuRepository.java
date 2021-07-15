a02a705d1b7b79d61b7e5c3e96130ed5776e89f8
MINJU-KIMmm <0112km@ewhain.net>
2021-07-15 18:23:42 +0900

    Fix : "Repository 어노테이션 추가"

--- src/main/java/com/matzipuniv/sinchon/domain/MenuRepository.java
+++ src/main/java/com/matzipuniv/sinchon/domain/MenuRepository.java
@@ -1,9 +1,11 @@
 package com.matzipuniv.sinchon.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface MenuRepository extends JpaRepository<Menu, Long> {
     List<Menu> findAllByRestaurant(Long restaurant);
 }
