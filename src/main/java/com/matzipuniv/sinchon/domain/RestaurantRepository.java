e010a1e91ffc634407186cb08dc3d43981395b3c
MINJU-KIMmm <0112km@ewhain.net>
2021-07-16 17:13:39 +0900

    Fix

--- src/main/java/com/matzipuniv/sinchon/domain/RestaurantRepository.java
+++
@@ -1,9 +0,0 @@
-
-package com.matzipuniv.sinchon.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
-}
