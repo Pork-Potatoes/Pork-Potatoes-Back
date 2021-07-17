64691339060087b5b57cbf359336a7f6ec883f43
siyeonkm <68330823+siyeonkm@users.noreply.github.com>
2021-07-17 17:03:50 +0900

    Feat: "유저 닉네임, 사진 수정 기능"

--- src/main/java/com/matzipuniv/sinchon/web/UserApiController.java
+++ src/main/java/com/matzipuniv/sinchon/web/UserApiController.java
@@ -3,18 +3,30 @@ package com.matzipuniv.sinchon.web;
 import com.matzipuniv.sinchon.service.UserService;
 import com.matzipuniv.sinchon.web.dto.UserResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.websocket.server.PathParam;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api/users")
 public class UserApiController {
     private final UserService userService;
 
-    @GetMapping("/api/users/{userNum}")
+    @GetMapping("/{userNum}")
     public UserResponseDto findByNum(@PathVariable Long userNum){
         return userService.findByNum(userNum);
+    }
+
+    @PatchMapping("/{userNum}/nickname")
+    public String updateNickname(@PathVariable Long userNum, @RequestBody String nickname) {
+        return userService.updateNickname(userNum, nickname);
+    }
+
+    @PatchMapping("/{userNum}/image")
+    public String updateProfile(@PathVariable Long userNum, @RequestParam("uploadFile")MultipartFile uploadFile) {
+        return userService.updateProfileUrl(userNum, uploadFile);
     }
 
 }
