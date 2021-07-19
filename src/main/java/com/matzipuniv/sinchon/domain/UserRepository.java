ea4777f6860fc25a683ce9e7cccc21faeb87188a
siyeonkm <68330823+siyeonkm@users.noreply.github.com>
2021-07-19 22:50:23 +0900

    Fix: "탈퇴관련 수정"
    탈퇴한 유저 검색안되게 하는 기능 및 탈퇴하면 즐찾삭제(진짜로 삭제)

--- src/main/java/com/matzipuniv/sinchon/domain/UserRepository.java
+++ src/main/java/com/matzipuniv/sinchon/domain/UserRepository.java
@@ -3,6 +3,10 @@ package com.matzipuniv.sinchon.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findAllByDeleteFlagFalse();
+    User findByUserNumAndDeleteFlagFalse(Long userNum);
 }
