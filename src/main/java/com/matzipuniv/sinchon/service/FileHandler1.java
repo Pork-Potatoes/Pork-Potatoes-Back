f2be21de5cb22fc51a420b9ce9565e6fc68145d2
seojunglee <71055448+seojunglee@users.noreply.github.com>
2021-07-20 22:08:24 +0900

    Review Post 기능 1차 구현

--- src/main/java/com/matzipuniv/sinchon/service/FileHandler1.java
+++ src/main/java/com/matzipuniv/sinchon/service/FileHandler1.java
@@ -8,81 +8,81 @@ import org.springframework.util.ObjectUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Component
 public class FileHandler1 {
-    private final ImageService imageService;
-
-    public FileHandler1(ImageService imageService) {
-        this.imageService = imageService;
-    }
-
     public List<Image> parseFileInfo(List<MultipartFile> multipartFiles) throws Exception {
-        List<Image> imageList = new ArrayList<>();
 
-        if (!CollectionUtils.isEmpty(multipartFiles)) {
-            LocalDateTime now = LocalDateTime.now();
-            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
-            String currentDate = now.format(dateTimeFormatter);
+        // 반환을 할 파일 리스트
+        List<Image> fileList = new ArrayList<>();
 
-            String absolutePath = new File("C:\\Users\\Sierra\\Desktop\\upload").getAbsolutePath()+ File.separator + File.separator;
+        // 파일이 빈 것이 들어오면 빈 것을 반환
+        if (multipartFiles.isEmpty()) {
+            return fileList;
+        }
 
-            String path = "images" + File.separator + currentDate;
-            File file = new File(path);
+        // 파일 이름을 업로드 한 날짜로 바꾸어서 저장할 것이다
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
+        String current_date = simpleDateFormat.format(new Date());
 
-            if (!file.exists()) {
-                boolean wasSuccessful = file.mkdir();
 
-                if (!wasSuccessful) {
-                    System.out.println("file : was not successful");
-                }
-            }
-            for (MultipartFile multipartFile : multipartFiles) {
-                String originalFileExtension;
+        // 프로젝트 폴더에 저장하기 위해 절대경로를 설정 (Window 의 Tomcat 은 Temp 파일을 이용한다)
+        String absolutePath = new File("C:\\Users\\Sierra\\Desktop\\upload").getAbsolutePath() + "\\";
+
+        // 경로를 지정하고 그곳에다가 저장
+        String path = "images/" + current_date;
+        File file = new File(absolutePath + path);
+        // 저장할 위치의 디렉토리가 존지하지 않을 경우
+        if (!file.exists()) {
+            // mkdir() 함수와 다른 점은 상위 디렉토리가 존재하지 않을 때 그것까지 생성
+            file.mkdirs();
+        }
+
+        // 파일들을 이제 만져볼 것이다
+        for (MultipartFile multipartFile : multipartFiles) {
+            // 파일이 비어 있지 않을 때 작업을 시작해야 오류가 나지 않는다
+            if (!multipartFile.isEmpty()) {
+                // jpeg, png, gif 파일들만 받아서 처리할 예정
                 String contentType = multipartFile.getContentType();
-
+                String originalFileExtension;
+                // 확장자 명이 없으면 이 파일은 잘못 된 것이다
                 if (ObjectUtils.isEmpty(contentType)) {
                     break;
                 } else {
-                    if (contentType.contains("image/jpeg"))
+                    if (contentType.contains("image/jpeg")) {
                         originalFileExtension = ".jpg";
-                    else if (contentType.contains("image/png"))
+                    } else if (contentType.contains("image/png")) {
                         originalFileExtension = ".png";
-                    else  // 다른 확장자일 경우 처리 x
+                    } else if (contentType.contains("image/gif")) {
+                        originalFileExtension = ".gif";
+                    }
+                    // 다른 파일 명이면 아무 일 하지 않는다
+                    else {
                         break;
+                    }
                 }
-                String newFileName = System.nanoTime() + originalFileExtension;
-
-                ImageResponseDto imageResponseDto = ImageResponseDto.builder()
+                // 각 이름은 겹치면 안되므로 나노 초까지 동원하여 지정
+                String new_file_name = Long.toString(System.nanoTime()) + originalFileExtension;
+                // 생성 후 리스트에 추가
+                Image image = Image.builder()
                         .originalFileName(multipartFile.getOriginalFilename())
-                        .filePath(path + File.separator + newFileName)
+                        .filePath(path + "//" + new_file_name)
                         .fileSize(multipartFile.getSize())
                         .build();
+                fileList.add(image);
 
-                Image image = new Image(
-                        imageResponseDto.getOriginalFileName(),
-                        imageResponseDto.getFilePath(),
-                        imageResponseDto.getFileSize()
-                );
-
-                imageList.add(image);
-
-                file = new File(absolutePath + path + File.separator + newFileName);
+                // 저장된 파일로 변경하여 이를 보여주기 위함
+                file = new File(path + "//" + new_file_name);
                 multipartFile.transferTo(file);
-
-                file.setWritable(true);
-                file.setReadable(true);
             }
-
-
         }
-            return imageList;
 
+        return fileList;
     }
-
-
 }
