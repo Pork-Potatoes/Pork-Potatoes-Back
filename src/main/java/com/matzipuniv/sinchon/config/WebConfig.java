555d3305326bcc265d1b82345cf2ddf5ead24ef9
hak2711 <hak271111@gmail.com>
2021-07-28 17:56:10 +0900

    Feat: "추가 CORS allowedMethod"

--- src/main/java/com/matzipuniv/sinchon/config/WebConfig.java
+++ src/main/java/com/matzipuniv/sinchon/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer{
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
                 .allowedOrigins("*", "http://localhost:3000")
-                .allowedMethods("GET")
+                .allowedMethods("*")
                 .maxAge(3000);
     }
 }
