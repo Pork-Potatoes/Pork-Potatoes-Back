eced09f97530f2b2601abf00aca0a86e524522d5
siyeonkm <68330823+siyeonkm@users.noreply.github.com>
2021-10-03 13:03:06 +0900

    Feat: "웹소켓 구현 1차완성"
    1. 서버랑 웹소켓 connect하기 완료
    2. 메세지 보내면 서버가 받는거 완료
    3. 일단 상대가 없으므로 보낸사람에게 가공한 메세지 다시 보내주기 완료

---
+++ src/main/java/com/matzipuniv/sinchon/config/MyHandler.java
@@ -0,0 +1,79 @@
+package com.matzipuniv.sinchon.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MyHandler extends TextWebSocketHandler {
+    private static final Logger logger = LoggerFactory.getLogger(WebSocketHandler.class);
+
+    //로그인 한 인원 전체
+    private List<WebSocketSession> sessions = new ArrayList<WebSocketSession>();
+    // 1:1로 할 경우
+    private Map<String, WebSocketSession> userSessionsMap = new HashMap<String, WebSocketSession>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {//클라이언트와 서버가 연결
+        // TODO Auto-generated method stub
+        logger.info("Socket 연결");
+        sessions.add(session);
+        logger.info(currentUserName(session));//현재 접속한 사람
+        userSessionsMap.put(currentUserName(session),session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String msg = message.getPayload();//자바스크립트에서 넘어온 Msg
+
+        if(msg != null) {
+            String[] msgs = msg.split(",");
+            if (msgs != null && msgs.length == 4) {
+                String bid = msgs[0];//게시물 번호
+                String receiver = msgs[1];//글 작성자
+                String count = msgs[2];//0이면 좋아요 취소 1이면 좋아요
+                String btitle = msgs[3];//게시물 제목
+                String comment = "";
+                if (count.equals("0")) {
+                    comment = "의 좋아요를 취소했습니다.";
+                } else if (count.equals("1")) {
+                    comment = "을 좋아합니다.";
+                }
+                String mid = currentUserName(session);//좋아요 누른 사람
+
+                WebSocketSession receiversession = userSessionsMap.get(receiver);//글 작성자가 현재 접속중인가 체크
+
+                if (receiversession != null) {
+                    TextMessage txtmsg = new TextMessage(mid + "님이" + receiver + "님의" + btitle + comment);
+                    receiversession.sendMessage(txtmsg);//작성자에게 알려줍니다
+                } else {
+                    TextMessage txtmsg = new TextMessage(mid + "님이" + receiver + "님의" + btitle + comment);
+                    session.sendMessage(txtmsg);//보내지는지 체크하기
+                }
+
+            }
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {//연결 해제
+        // TODO Auto-generated method stub
+        logger.info("Socket 끊음");
+        sessions.remove(session);
+        userSessionsMap.remove(currentUserName(session),session);
+    }
+
+    private String currentUserName(WebSocketSession session) {
+        String mid = session.getId();
+        //String mid = session.getPrincipal().getName();
+        return mid;
+    }
+}
---
+++ src/main/java/com/matzipuniv/sinchon/config/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package com.matzipuniv.sinchon.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@Controller
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(myHandler(), "/myHandler").setAllowedOrigins("*")
+                .withSockJS();
+    }
+
+    @Bean
+    public WebSocketHandler myHandler() {
+        return new MyHandler();
+    }
+}
